6d3856649fb300fc5f233328811ae3605c20069b
Charles Moyes <TheChuckster@gmail.com>
2012-03-20 19:32:44 -0400

    ADC program

--- lab5/lab5/lab5.c
+++ lab5/lab5/lab5.c
@@ -1,98 +1,97 @@
-// This pgm just blinks D.2 for testing the protoboard.
 #include <inttypes.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <avr/sleep.h>
 #include <stdio.h>
-
-// serial communication library
+#include <stdlib.h>
+#include <util/delay.h>
 #include "uart.h"
+
 // UART file descriptor
 // putchar and getchar are in uart.c
 FILE uart_str = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW);
-  
 
-//timeout values for each task
-#define t1 200   
-#define begin {
-#define end }
- 
-// task subroutines
-void task1(void);  	//blink at 2 or 8 Hz 
-void initialize(void); //all the usual mcu stuff 
-          
-volatile unsigned char time1;	//timeout counter 
-unsigned char led;				//light states  
-unsigned int ticks ;			// running time
-         
-//**********************************************************
-//timer 0 compare ISR
+// timeout values for each task
+#define t1 100
+
+volatile unsigned char time1; // timeout counter
+unsigned char led; // light states
+
+volatile int Ain, AinLow; // raw A to D number
+
+// timer 0 compare ISR
 ISR (TIMER0_COMPA_vect)
-begin      
-  //Decrement the  time if they are not already zero
-  if (time1>0)	--time1;
-end  
+{
+	if (time1 > 0) --time1;
+	
+	if (time1 == 0)
+	{
+		time1 = t1;
 
-//**********************************************************       
-//Entry point and task scheduler loop
-int main(void)
-begin  
-  	initialize();
-  
-  	//main task scheduler loop 
-  	while(1)
-  	begin     
-		if (time1==0){time1=t1;	task1();}
-  	end
-end  
-  
-//**********************************************************          
-//Task 1
-void task1(void) 
-begin 
- 
-  	//toggle the second bit
-  	led = led ^ 1 ;
-  	PORTD = (led<<PORTD2) ;
+		// toggle the LED and the square wave test output
+		led ^= 1;
+		PORTD = (led << PORTD2) | (led << PORTD3);
+	}
+}
 
-	// print time to test USART
-	printf("%d\n\r", ticks++);
+void pwm_init()
+{
+	// Set timer/counter 1 to use 10-bit PWM mode.
+	// The counter counts from zero to 1023 and then back down
+	// again. Each time the counter value equals the value
+	// of OCR1(A), the output pin is toggled.
+	// The counter speed is set in TCCR1B, to clk / 256 = 28800Hz.
+	// Effective frequency is then clk / 256 / 2046 = 14 Hz
 
-	// get a single character to test USART
-	if (UCSR0A & (1<<RXC0))
-	begin
-		printf("you typed...%c\n\r", UDR0);
-	end
-end  
- 
+	OCR1A = 512;
+	TCCR1A = (1 << COM1A1) | (1 << WGM11) | (1 << WGM10);
+	TCCR1B = (1 << CS12);
+}
 
-//********************************************************** 
-//Set it all up
-void initialize(void)
-begin
-	//set up the LED port
-  	DDRD = (1<<PORTD2) ;	// PORT D.2 is an ouput
-           
-  	//set up timer 0 for 1 mSec timebase 
-  	TIMSK0= (1<<OCIE0A);	//turn on timer 0 cmp match ISR 
-  	OCR0A = 249;  		//set the compare register to 250 time ticks
-  	//set prescalar to divide by 64 
-  	TCCR0B= 3; 	
-  	// turn on clear-on-match
-  	TCCR0A= (1<<WGM01) ;
-    
-  	//init the LED status 
-  	led=0x00; 
-  
-  	//init the task timer
-  	time1=t1;  
-    
-	//init the UART -- uart_init() is in uart.c
-  	uart_init();
-  	stdout = stdin = stderr = &uart_str;
-  	fprintf(stdout,"Starting...\n\r");
-    
-  	//crank up the ISRs
-  	sei();
-end  
+int main()
+{
+	// init the A to D converter 
+	//channel zero/ right adj /ext Aref
+	//!!!DO NOT CONNECT Aref jumper!!!!
+	ADMUX = (1<<REFS0); 
+	//enable ADC and set prescaler to 1/127*16MHz=125,000
+	//and set int enable
+	ADCSRA = (1<<ADEN) + 7 ; 
 
+	// set up the LED port
+  	DDRD = (1 << PORTD2) | (1 << PORTD3); // PORT D.2 is an ouput
+
+  	// set up timer 0 for 1 mSec timebase
+  	TIMSK0 = 1 << OCIE0A; // turn on timer 0 cmp match ISR
+  	OCR0A = 249; // set the compare register to 250 time ticks
+  	TCCR0B = 3; // set prescalar to divide by 64
+  	TCCR0A = 1 << WGM01; // turn on clear-on-match
+  	
+  	// set up timer for PWM
+  	pwm_init();
+
+  	led = 0x00; // init the LED status
+
+  	time1 = t1; // init the task timer
    
+	// init the UART -- uart_init() is in uart.c
+	uart_init();
+	stdout = stdin = stderr = &uart_str;
+	fprintf(stdout,"\n\rStarting ADC ISR demo...\n\r"); 
+
+	sei();
+
+	// measure and display loop
+	while (1)
+	{    
+		// start another conversion
+		ADCSRA |= (1<<ADSC);
+
+		AinLow = (int)ADCL;
+		Ain = (int)ADCH*256; 
+		Ain = Ain + AinLow;
+
+		// program ONLY gets here after ADC ISR is done
+		fprintf(stdout, "%d\n\r", Ain);
+	}
+}
